c1c8bb6ccd38e8ab4f07bfd7605d8776aec5d70a
21343054 <112170832+21343054@users.noreply.github.com>
2022-10-18 18:15:56 +0700

    prak7

---
+++ Do_While3.java
@@ -0,0 +1,19 @@
+/*
+ * Nama : Maulana Hafizul Haq
+ * NIM  : 21343054
+ * Prodi: Teknik Informatika
+ */
+package Latihan;
+
+public class Do_While3 {
+   public static void main(String []args) {
+    int i = 20;
+   
+   do
+   {
+    System.out.println(i);
+    i--;
+   }
+   while (i > 10);
+}
+}
---
+++ For3.java
@@ -0,0 +1,14 @@
+/*
+ * Nama : Maulana Hafizul Haq
+ * NIM  : 21343054
+ * Prodi: Teknik Informatika
+ */
+package Latihan;
+
+public class For3 {
+    public static void main(String []args) {
+        int bil;
+        for(bil = 0 ; bil <= 10 ; bil++)
+        System.out.println(bil);
+    }
+}
